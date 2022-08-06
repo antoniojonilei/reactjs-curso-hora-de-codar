2d1c3e5a4b86e4ce79fdc7100329eb6a649a7826
Antonio Jonilei <100094268+antoniojonilei@users.noreply.github.com>
2022-08-06 19:06:40 -0300

    fisrt commit

---
+++ 00-fundamentos/fundamentos/src/App.js
@@ -0,0 +1,15 @@
+//components
+import FisrtComponent from "./components/FirstComponent"
+//style/ CSS
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos</h1>
+      <FisrtComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ 00-fundamentos/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,11 @@
+function FisrtComponent() {
+
+    return(
+        <div>
+            <h1>Meu primeiro componente</h1>
+        </div>
+    )
+
+}
+
+export default FisrtComponent
